e2ed711395c83e4004620430497c2c1ac807bbfe
DyadyaSasha <serebryakovalexx@yandex.ru>
2016-11-29 04:13:23 +0300

    some changes

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
-from django.shortcuts import redirect
 from django.contrib.auth.models import User
 
 # Create your views here.
